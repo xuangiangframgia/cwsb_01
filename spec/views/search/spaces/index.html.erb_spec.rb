b5e818ed804f99bd5a485553d9e589e5f8e3b342
ledinhdoanframgia <le.dinh.doan@framgia.com>
2016-11-03 16:39:41 +0700

    Rspec_controller_venue

--- spec/views/search/spaces/index.html.erb_spec.rb
+++ spec/views/search/spaces/index.html.erb_spec.rb
@@ -9,5 +9,13 @@ RSpec.describe "search/spaces/index.html.erb", type: :view do
     it {expect(rendered).to match /Maps/}
     it {expect(rendered).to match /Find Space/}
     it {expect(rendered).to match /id="map"/}
+    it {expect(rendered).to match /Desk/}
+    it {expect(rendered).to match /Prive_office/}
+    it {expect(rendered).to match /Meeting_room/}
+    it {expect(rendered).to match /day/}
+    it {expect(rendered).to match /week/}
+    it {expect(rendered).to match /month/}
+    it {expect(rendered).to match /Space type/}
+    it {expect(rendered).to match /Term/}
   end
 end
